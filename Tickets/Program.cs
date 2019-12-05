4c0b3ccbd4598f2a7c552700dd29b16afdf31ca9
RODRIGO GONCALVES SANTANA <rodrigo.gsantana1@senacsp.edu.br>
2019-12-05 08:32:42 -0300

    primeira versao

---
+++ Tickets/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Tickets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Sistema de validação de Tickets para entrada em Teatro/Festas.                
+             */
+        }
+    }
+}
