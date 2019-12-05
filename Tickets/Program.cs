648fccbee3af7f8a2658b0fab6178a1a2c3bcfd0
RODRIGO GONCALVES SANTANA <rodrigo.gsantana1@senacsp.edu.br>
2019-12-05 11:49:29 -0300

    Sistema Pronto

--- Tickets/Program.cs
+++ Tickets/Program.cs
@@ -2,18 +2,39 @@
 
 namespace Tickets
 {
-   class Program
-   {
-      static void Main(string[] args)
-      {
-         string[] tickets = { "580624", "560140", "560137", "560101", "631497", "560133" };
-         Console.WriteLine("Sistema de Validação de Tickets");
-         Console.WriteLine();
-         Console.Write("Informe o ticket: ");
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tickets = { "580624", "560140", "560137", "560101", "631497", "560133" };
+            bool ticketValido;
 
-         //Captura o ticket informado
-         string ticket = Console.ReadLine();
+            while (true)
+            {
+                Console.WriteLine("Sistema de Validação de Tickets");
+                Console.WriteLine();
+                Console.Write("Informe o ticket: ");
+                string ticket = Console.ReadLine();
+                ticketValido = false;
 
-      }
-   }
+                for (int item = 0; item < tickets.Length; item++)
+                {
+                    if (tickets[item] == ticket)
+                    {
+                        ticketValido = true;
+                        tickets[item] = null;
+                        break;
+                    }
+                }
+
+                if (ticketValido)
+                    Console.WriteLine("Ticket Válido. Entrada Liberada!");
+                else
+                    Console.WriteLine("Ticket Inválido. Entrada Bloqueada!");
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
 }
