2e23c197729cd58beaa4260aadfd914c61efce60
Jose Laercio <jose.lrlima2@senacsp.edu.br>
2019-12-05 09:48:05 -0300

    Captura o ticket informado

--- Tickets/Program.cs
+++ Tickets/Program.cs
@@ -2,16 +2,18 @@
 
 namespace Tickets
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-          
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         string[] tickets = { "580624", "560140", "560137", "560101", "631497", "560133" };
+         Console.WriteLine("Sistema de Validação de Tickets");
+         Console.WriteLine();
+         Console.Write("Informe o ticket: ");
 
-            string[] tickets = { "580624", "560140", "560137", "560101", "631497", "560133" };
-            Console.WriteLine("Sistema de Validação de Tickets");
-            Console.WriteLine();
-            Console.Write("Informe o ticket: ");
-        }
-    }
+         //Captura o ticket informado
+         string ticket = Console.ReadLine();
+
+      }
+   }
 }
